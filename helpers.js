ebc9790b434862be3ae9a50ddf81415607db16d3
Steven Gallo <steven.gallo@outook.com>
2017-02-15 08:39:45 -0500

    fixed

--- helpers.js
+++ helpers.js
@@ -2,15 +2,14 @@ const axios = require('axios');
 require('dotenv').config();
 
 function getLocation(req, res, next) {
-    axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GKEY}`)
+    axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GGKEY}`)
         .then((gRes) => {
-            console.log(`google response: ${gRes}`)
             res.locals.latitude = gRes.data.location.lat
             res.locals.longitude = gRes.data.location.lng
             return next()
         }).catch((err) => {
-            res.locals.latitude = '37.8267'
-            res.locals.longitude = '-122.4233'
+            res.locals.latitude = '40.7411155'
+            res.locals.longitude = '-73.9893962'
             return next()
         })
 }
@@ -21,9 +20,24 @@ function getAddLocation(req, res, next) {
         .then((gRes) => {
             req.session.latitude = gRes.data.results[0].geometry.location.lat
             req.session.longitude = gRes.data.results[0].geometry.location.lng
+            req.session.newLocation = gRes.data.results[0].address_components[2].long_name
+            console.log(`${req.session.newLocation} new`)
             return next()
         }).catch((err) => { console.log(err) })
 
 }
 
-module.exports = { getLocation, getAddLocation };
+function getCity(req, res, next) {
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${res.locals.latitude},${res.locals.longitude}&key=${process.env.GKEY}`)
+        .then((gRes) => {
+            req.session.location = gRes.data.results[0].address_components[2].long_name
+            console.log(`${req.session.location} orig`)
+            return next()
+        }).catch((err) => { console.log(err) })
+}
+
+module.exports = {
+    getLocation,
+    getAddLocation,
+    getCity
+};
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,22 @@ const axios = require('axios')
 const helpers = require('../helpers');
 require('dotenv').config();
 
-router.get('/', helpers.getLocation, (req, res, next) => {
+router.get('/', helpers.getLocation, helpers.getCity, (req, res, next) => {
     let latitude = req.session.latitude | res.locals.latitude
     let longitude = req.session.longitude | res.locals.longitude
+    let location = ''
+    if (req.session.newLocation) {
+        location = req.session.newLocation
+    } else {
+        location = req.session.location
+    }
     axios.get(`https://api.darksky.net/forecast/${process.env.DKEY}/${latitude},${longitude}`)
         .then((gRes) => {
             res.render('index', {
                 current: gRes.data.currently,
                 hourly: gRes.data.hourly,
-                daily: gRes.data.daily
+                daily: gRes.data.daily,
+                location: location
             })
         }).catch((err) => { console.log(err) })
 });
